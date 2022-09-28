88361d97c47f1aae230fd6b80d3069917deb1c58
Art1903 <artemteliuk@gmail.com>
2022-09-28 20:20:16 +0300

    Add media query on main page for telephone

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
@@ -6,3 +5,11 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+let header__burger = document.querySelector('.header__burger');
+let header__menu = document.querySelector('.header__menu');
+
+header__burger.addEventListener('click', function(){
+	header__burger.classList.toggle('active');
+	header__menu.classList.toggle('active');
+})
